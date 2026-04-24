9f230509a89664c32382a86f40c59e5635388f0f
Sagnik Biswas <ic23btech11017@iith.ac.in>
2026-04-24 14:06:27 +0530

    Fix TypeScript PORT type error

--- api/index.ts
+++ api/index.ts
@@ -104,7 +104,7 @@ function getNextId(tableArray: any[]) {
 }
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(cors());
 app.use(express.json());
